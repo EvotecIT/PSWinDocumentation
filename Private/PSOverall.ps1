f73ce648bc961cc1906a9c8d5500f2521a5ef2ce
PrzemyslawKlys <pklys@evotec.pl>
2018-07-19 16:54:08 +0200

    Update

--- Private/PSOverall.ps1
+++ Private/PSOverall.ps1
@@ -66,12 +66,10 @@ function Get-ComputerCulture {
     param(
         $ComputerName = $Env:COMPUTERNAME
     )
-
     $ScriptBlock = {
         get-culture | select KeyboardLayoutId, DisplayName, @{Expression = {$_.ThreeLetterWindowsLanguageName}; Label = "Windows Language"}
     }
     if ($ComputerName -eq $Env:COMPUTERNAME) {
-
         $Data8 = Invoke-Command -ScriptBlock $ScriptBlock
     } else {
         $Data8 = Invoke-Command -ComputerName $ComputerName -ScriptBlock $ScriptBlock
