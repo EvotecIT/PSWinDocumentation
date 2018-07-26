5bf9587a35cc5967695ea602b1a21ab2a0e3602b
PrzemyslawKlys <pklys@evotec.pl>
2018-07-26 12:08:53 +0200

    Update

--- Examples/Run-Server.ps1
+++ Examples/Run-Server.ps1
@@ -11,54 +11,89 @@ function Start-WinDocumentationServer {
         [string[]] $ComputerName = $Env:COMPUTERNAME,
         [string] $FilePathTemplate,
         [string] $FilePath,
-        [switch] $OpenDocument
+        [switch] $OpenDocument,
+        $CompanyName = 'Evotec'
     )
+
     $WordDocument = New-WordDocument -FilePath $FilePath
     $ListOfHeaders = @(
         'Scope',
         'General Information',
         'General Information - Forest Summary',
-        'General Information - Domain Summary')
+        'General Information - Domain Summary'
+        'General Information - UPN Summary'
+        'General Information - UPN Summary'
+    )
+    $ListOfHeaders1 = [ordered] @{
 
-    $Toc = Add-WordToc -WordDocument $WordDocument -Title 'Table of content' -Switches C, A -RightTabPos 15 -HeaderStyle Heading1
+    }
+
+    # $Toc = Add-WordToc -WordDocument $WordDocument -Title 'Table of content' -Switches C, A -RightTabPos 15 -HeaderStyle Heading1
 
     ### This list will be converted into Headings for Numbered Table of Contents
     $ListHeaders = Add-WordList -WordDocument $WordDocument -ListType Numbered -ListData $ListOfHeaders
     ### Converts List into numbered Headings for Table of Content
     $Headings = Convert-ListToHeadings -WordDocument $WordDocument -List $ListHeaders
-    $Text = "This document provides a low-level design of roles and permissions for the IT infrastructure team at Colmore for a 3 level support organization. This document utilizes knowledge from AD General Concept document that should be delivered with this document. Having all the information described in attached document one can start designing Active Directory with those principles in mind. It's important to know while best practices that were described are important in decision making they should not be treated as final and only solution. Most important aspect is to make sure company has full usability of Active Directory and is happy with how it works. Making things harder just for the sake of implementation of best practices isn't always the best way to go."
+
+    $Text = "This document provides a low-level design of roles and permissions for the IT infrastructure team at $CompanyName organization. This document utilizes knowledge from AD General Concept document that should be delivered with this document. Having all the information described in attached document one can start designing Active Directory with those principles in mind. It's important to know while best practices that were described are important in decision making they should not be treated as final and only solution. Most important aspect is to make sure company has full usability of Active Directory and is happy with how it works. Making things harder just for the sake of implementation of best practices isn't always the best way to go."
     $Section1 = $Headings[0]
     #$Section1 = Add-WordParagraph -WordDocument $WordDocument -Paragraph $Section1 -AfterSelf -Supress $false
     $Section1 = Add-WordText -WordDocument $WordDocument -Paragraph $Section1 -Text $Text -Alignment both
 
     ### 3rd section - Forest Summary
-    $ActiveDirectorySnapshot = Get-ActiveDirectoryData
-    $ActiveDirectoryForestInformation = $ActiveDirectorySnapshot.ForestInformation
-    $ActiveDirectoryForestFeatures = $ActiveDirectorySnapshot.ForestFeatures
-    $ActiveDirectoryFSMO = $ActiveDirectorySnapshot.FSMO
-
     $Section3Paragraph1 = $Headings[2]
-    $Section3Paragraph1 = Add-WordText -WordDocument $WordDocument -Paragraph $Section3Paragraph1 -Text 'Following table contains Forest Summary'
-    $Section3Table1 = Add-WordTable -WordDocument $WordDocument -Paragraph $Section3Paragraph1 -DataTable $ActiveDirectoryForestInformation -AutoFit Window -DoNotAddTitle  -Design TableGrid
+
+    $ActiveDirectorySnapshot = Get-ActiveDirectoryProcessedData
+    $ForestName = $($ActiveDirectorySnapshot.ForestInformation.Name)
+    $ForestNameDN = $($ActiveDirectorySnapshot.RootDSE.defaultNamingContext)
+
+    $ForestSummaryText = "Active Directory at $CompanyName has a forest name $ForestName ($ForestNameDN). Following table contains forest summary with important information:"
+    $Section3Paragraph1 = Add-WordText -WordDocument $WordDocument -Paragraph $Section3Paragraph1 -Text $ForestSummaryText
+    $Section3Table1 = Add-WordTable -WordDocument $WordDocument -Paragraph $Section3Paragraph1 -DataTable $ActiveDirectorySnapshot.ForestInformation -AutoFit Window -DoNotAddTitle  -Design TableGrid
     $Section3Table1 = Set-WordTableRowMergeCells -Table $Section3Table1 -RowNr 0 -ColumnNrStart 0 -ColumnNrEnd 1
     $Section3Table1Paragraph = Get-WordTableRow -Table $Section3Table1 -RowNr 0 -ColumnNr 0
     $Section3Table1Paragraph = Add-WordText -WordDocument $WordDocument -Paragraph $Section3Table1Paragraph -Text 'Forest Summary' -Alignment center -Color Black -Append
 
-    $Section3Paragraph2 = Add-WordParagraph -WordDocument $WordDocument -Table $Section3Table1 -AfterSelf
+    $Section3Paragraph2 = Add-WordParagraph -WordDocument $WordDocument -Table $Section3Table1 -InsertWhere AfterSelf
     $Section3Paragraph2 = Add-WordText -WordDocument $WordDocument -Paragraph $Section3Paragraph2 -Text 'Following table contains Forest Features'
-    $Section3Table2 = Add-WordTable -WordDocument $WordDocument -Paragraph $Section3Paragraph2 -DataTable $ActiveDirectoryForestFeatures -AutoFit Window -DoNotAddTitle -Design TableGrid
+    $Section3Table2 = Add-WordTable -WordDocument $WordDocument -Paragraph $Section3Paragraph2 -DataTable $ActiveDirectorySnapshot.OptionalFeatures -AutoFit Window -DoNotAddTitle -Design TableGrid
     $Section3Table2 = Set-WordTableRowMergeCells -Table $Section3Table2 -RowNr 0 -ColumnNrStart 0 -ColumnNrEnd 1
     $Section3Table2Paragraph = Get-WordTableRow -Table $Section3Table2 -RowNr 0 -ColumnNr 0
-    $Section3Table2Paragraph = Add-WordText -WordDocument $WordDocument -Paragraph $Section3Table2Paragraph -Text 'Forest Summary' -Alignment center -Color Black -Append
+    $Section3Table2Paragraph = Add-WordText -WordDocument $WordDocument -Paragraph $Section3Table2Paragraph -Text 'Forest Features' -Alignment center -Color Black -Append
 
-    $Section3Paragraph3 = Add-WordParagraph -WordDocument $WordDocument -Table $Section3Table2 -AfterSelf
+    $Section3Paragraph3 = Add-WordParagraph -WordDocument $WordDocument -Table $Section3Table2 -InsertWhere AfterSelf
     $Section3Paragraph3 = Add-WordText -WordDocument $WordDocument -Paragraph $Section3Paragraph3 -Text 'Following table contains FSMO servers'
-    $Section3Table3 = Add-WordTable -WordDocument $WordDocument -Paragraph $Section3Paragraph3 -DataTable $ActiveDirectoryFSMO -AutoFit Window -DoNotAddTitle -Design TableGrid
+    $Section3Table3 = Add-WordTable -WordDocument $WordDocument -Paragraph $Section3Paragraph3 -DataTable $ActiveDirectorySnapshot.FSMO -AutoFit Window -DoNotAddTitle -Design TableGrid
     $Section3Table3 = Set-WordTableRowMergeCells -Table $Section3Table3 -RowNr 0 -ColumnNrStart 0 -ColumnNrEnd 1
     $Section3Table2Paragraph = Get-WordTableRow -Table $Section3Table3 -RowNr 0 -ColumnNr 0
     $Section3Table3Paragraph = Add-WordText -WordDocument $WordDocument -Paragraph $Section3Table2Paragraph -Text 'FSMO Roles' -Alignment center -Color Black -Append
 
+    ### Section 4 - Domain Summary
+    $Section4Paragraph1 = $Headings[3]
 
+    $DomainNetBios = $ActiveDirectorySnapshot.DomainInformation.NetBIOS
+    $DomainName = $ActiveDirectorySnapshot.DomainInformation.DNSRoot
+    $DomainDistinguishedName = $ActiveDirectorySnapshot.DomainInformation.DistinguishedName
+
+    $Text = "Following domains exists within forest $ForestName"
+    $Text0 = "Domain $DomainDistinguishedName"
+    $Text1 = "Name for fully qualified domain name (FQDN): $DomainName"
+    $Text2 = "Name for NetBIOS: $DomainNetBios"
+
+    $Section4Paragraph1 = Add-WordText -WordDocument $WordDocument -Paragraph $Section4Paragraph1 -Text $Text
+
+    $ListDomainInformation = $null
+    $ListDomainInformation = $ListDomainInformation | New-WordListItem -WordDocument $WordDocument -ListLevel 0 -ListItemType Bulleted -ListValue $Text0
+    $ListDomainInformation = $ListDomainInformation | New-WordListItem -WordDocument $WordDocument -ListLevel 1 -ListItemType Bulleted -ListValue $Text1
+    $ListDomainInformation = $ListDomainInformation | New-WordListItem -WordDocument $WordDocument -ListLevel 1 -ListItemType Bulleted -ListValue $Text2
+    Add-WordListItem -WordDocument $WordDocument -Paragraph $Section4Paragraph1 -List $ListDomainInformation -Verbose -Supress $true
+
+
+    ### Section 5 - UPN Summary
+    $Section5Paragraph1 = $Headings[4]
+    $Section5Paragraph1Text = "Following UPN suffixes were created for $CompanyName users and used as part of users logon processes:"
+    $Section5Paragraph1 = Add-WordText -WordDocument $WordDocument -Paragraph $Section5Paragraph1 -Text $Section5Paragraph1Text
+    Add-WordList -WordDocument $WordDocument -ListType Bulleted -Paragraph $Section5Paragraph1 -ListData $ActiveDirectorySnapshot.UPNSuffixes -Verbose
 
     <# Implment
     $ColorShading1 = Get-ColorFromARGB 255 0 112 192
@@ -106,4 +141,4 @@ function Start-WinDocumentationServer {
     if ($OpenDocument) { Invoke-Item $FilePath }
 }
 
-Start-WinDocumentationServer -ComputerName 'AD1' -FilePathTemplate $FilePathTemplate -FilePath $FilePath #-OpenDocument
+Start-WinDocumentationServer -ComputerName 'AD1' -FilePathTemplate $FilePathTemplate -FilePath $FilePath -OpenDocument
--- Private/PSAD.ps1
+++ Private/PSAD.ps1
@@ -1,133 +1,155 @@
 #Import-Module ActiveDirectory
-function Get-ActiveDirectoryData {
-    param(
-
-    )
+function Get-ActiveDirectoryCleanData {
+    param()
     $ADSnapshot = @{}
-    $ADSnapshot.RootDSE = $(
-        $Info = Get-ADRootDSE
-        <#
-            configurationNamingContext
-            currentTime
-            defaultNamingContext
-            dnsHostName
-            domainControllerFunctionality
-            domainFunctionality
-            dsServiceName
-            forestFunctionality
-            highestCommittedUSN
-            isGlobalCatalogReady
-            isSynchronized
-            ldapServiceName
-            namingContexts
-            rootDomainNamingContext
-            schemaNamingContext
-            serverName
-            subschemaSubentry
-            supportedCapabilities
-            supportedControl
-            supportedLDAPPolicies
-            supportedLDAPVersion
-            supportedSASLMechanisms
-            #>
-        #$Info
-        $Info | Select-Object `
-        @{label = 'Configuration Naming Context'; expression = { $_.configurationNamingContext }},
-        @{label = 'Default Naming Context'; expression = { $_.defaultNamingContext }},
-        @{label = 'DNS Host Name'; expression = { $_.dnsHostName }},
-        @{label = 'Domain Controller Functionality'; expression = { $_.domainControllerFunctionality }},
-        @{label = 'Domain Functionality'; expression = { $_.domainFunctionality }},
-        @{label = 'Forest Functionality'; expression = { $_.forestFunctionality }},
-        @{label = 'Supported LDAP Policies'; expression = { $_.supportedLDAPPolicies }},
-        @{label = 'Sub Schema Subentry'; expression = { $_.subschemaSubentry }},
-        @{label = 'Supported LDAP Version'; expression = { $_.supportedLDAPVersion }},
-        @{label = 'Supported SASL Mechanisms'; expression = { $_.supportedSASLMechanisms }}
-
-    )
-    $ADSnapshot.FSMO = $( #Flexible Single Master Operations
-        #$Info1 = Get-ADRootDSE | Select-Object DomainNamingMaster
-        $Info2 = Get-ADDomain | Select-Object PDCEmulator, RIDMaster, InfrastructureMaster
-        $Info3 = Get-ADForest | Select-Object SchemaMaster, DomainNamingMaster
-        $Info = @{
-            'Domain Naming Master'  = $Info3.DomainNamingMaster
-            'Schema Master'         = $Info3.SchemaMaster
-            'PDC Emulator'          = $Info2.PDCEmulator
-            'RID Master'            = $Info2.RIDMaster
-            'Infrastructure Master' = $Info2.InfrastructureMaster
-        }
-        $Info
-    )
-    $ADSnapshot.ForestInformation = $(
-        $Info = Get-ADForest
-        <#
-            ApplicationPartitions
-            CrossForestReferences
-            DomainNamingMaster
-            Domains
-            ForestMode
-            GlobalCatalogs
-            Name
-            PartitionsContainer
-            RootDomain
-            SchemaMaster
-            Sites
-            SPNSuffixes
-            UPNSuffixes
-        #>
-
-        $Info | Select-Object Name, `
-        @{label = 'Forest Mode'; expression = { $_.ForestMode }},
-        @{label = 'Domain Naming Master'; expression = { $_.DomainNamingMaster }},
-        Domains,
-        Sites,
-        SPNSuffixes,
-        UPNSuffixes,
-        SchemaMaster,
-        RootDomain
-    )
-    $ADSnapshot.ForestFeatures = $(
-        $Info = @{ 'Recycle Bin Enabled' = "$(Get-ADOptionalFeature -Filter 'name -like "Recycle Bin Feature"').EnabledScopes)" -eq '' }
-        $Info
-    )
-
-
-    $ADSnapshot.DomainInformation = $(
-        Get-ADDomain
-    )
-    $ADSnapshot.DomainControllers = $(Get-ADDomainController -Filter *)
-    $ADSnapshot.DomainTrusts = (Get-ADTrust -Filter *)
+    <# RootDSE
+                configurationNamingContext
+                currentTime
+                defaultNamingContext
+                dnsHostName
+                domainControllerFunctionality
+                domainFunctionality
+                dsServiceName
+                forestFunctionality
+                highestCommittedUSN
+                isGlobalCatalogReady
+                isSynchronized
+                ldapServiceName
+                namingContexts
+                rootDomainNamingContext
+                schemaNamingContext
+                serverName
+                subschemaSubentry
+                supportedCapabilities
+                supportedControl
+                supportedLDAPPolicies
+                supportedLDAPVersion
+                supportedSASLMechanisms
+    #>
+    $ADSnapshot.RootDSE = $(Get-ADRootDSE)
+    $ADSnapshot.ForestInformation = $(Get-ADForest)
+    $ADSnapshot.DomainInformation = $(Get-ADDomain)
+    $ADSnapshot.DomainControllers = $(Get-ADDomainController -Filter * )
+    $ADSnapshot.DomainTrusts = (Get-ADTrust -Filter * )
     $ADSnapshot.DefaultPassWordPoLicy = $(Get-ADDefaultDomainPasswordPolicy)
     $ADSnapshot.AuthenticationPolicies = $(Get-ADAuthenticationPolicy -LDAPFilter '(name=AuthenticationPolicy*)')
     $ADSnapshot.AuthenticationPolicySilos = $(Get-ADAuthenticationPolicySilo -Filter 'Name -like "*AuthenticationPolicySilo*"')
-    $ADSnapshot.CentralAccessPolicies = $(Get-ADCentralAccessPolicy -Filter *)
-    $ADSnapshot.CentralAccessRules = $(Get-ADCentralAccessRule -Filter *)
-    $ADSnapshot.ClaimTransformPolicies = $(Get-ADClaimTransformPolicy -Filter *)
-    $ADSnapshot.ClaimTypes = $(Get-ADClaimType -Filter *)
+    $ADSnapshot.CentralAccessPolicies = $(Get-ADCentralAccessPolicy -Filter * )
+    $ADSnapshot.CentralAccessRules = $(Get-ADCentralAccessRule -Filter * )
+    $ADSnapshot.ClaimTransformPolicies = $(Get-ADClaimTransformPolicy -Filter * )
+    $ADSnapshot.ClaimTypes = $(Get-ADClaimType -Filter * )
     $ADSnapshot.DomainAdministrators = $( Get-ADGroup -Identity $('{0}-512' -f (Get-ADDomain).domainSID) | Get-ADGroupMember -Recursive)
-    $ADSnapshot.OrganizationalUnits = $(Get-ADOrganizationalUnit -Filter *)
-    $ADSnapshot.OptionalFeatures = $(Get-ADOptionalFeature -Filter *)
-    $ADSnapshot.Sites = $(Get-ADReplicationSite -Filter *)
-    $ADSnapshot.Subnets = $(Get-ADReplicationSubnet -Filter *)
-    $ADSnapshot.SiteLinks = $(Get-ADReplicationSiteLink -Filter *)
+    $ADSnapshot.OrganizationalUnits = $(Get-ADOrganizationalUnit -Filter * )
+    $ADSnapshot.OptionalFeatures = $(Get-ADOptionalFeature -Filter * )
+    $ADSnapshot.Sites = $(Get-ADReplicationSite -Filter * )
+    $ADSnapshot.Subnets = $(Get-ADReplicationSubnet -Filter * )
+    $ADSnapshot.SiteLinks = $(Get-ADReplicationSiteLink -Filter * )
     $ADSnapshot.LDAPDNS = $(Resolve-DnsName -Name "_ldap._tcp.$((Get-ADDomain).DNSRoot)" -Type srv)
     $ADSnapshot.KerberosDNS = $(Resolve-DnsName -Name "_kerberos._tcp.$((Get-ADDomain).DNSRoot)" -Type srv)
-    $ADSnapshot.GroupPolicies = $(
-        $Info = Get-GPO -All | Select DisplayName, Owner, DomainName, CreationTime, ModificationTime, GpoStatus, WmiFilter, Description # Id, UserVersion, ComputerVersion
-        $Info | Select-Object DisplayName, CreationTime, ModificationTime, GpoStatus, WmiFilter, Description
-    )
+    $ADSnapshot.GroupPolicies = $(Get-GPO -All) # DisplayName, Owner, DomainName, CreationTime, ModificationTime, GpoStatus, WmiFilter, Description # Id, UserVersion, ComputerVersion
     return $ADSnapshot
 }
-Clear-Host
-$ADSnapshot = Get-ActiveDirectoryData
-Write-Color 'Forest Information' -Color Red
-$ADSnapshot.ForestInformation
-Write-Color 'RootDSE' -Color Red
-$ADSnapshot.RootDSE
-Write-Color 'Domain Information' -Color Red
+
+function Get-ActiveDirectoryProcessedData {
+    $ADSnapshot = Get-ActiveDirectoryCleanData
+
+    $DisplayAD = @{}
+    $DisplayAD.RootDSE = $ADSnapshot.RootDSE
+    $DisplayAD.DomainInformation = $ADSnapshot.DomainInformation
+    $DisplayAD.FSMO = [ordered] @{
+        'Domain Naming Master'  = $ADSnapshot.ForestInformation.DomainNamingMaster
+        'Schema Master'         = $ADSnapshot.ForestInformation.SchemaMaster
+        'PDC Emulator'          = $ADSnapshot.DomainInformation.PDCEmulator
+        'RID Master'            = $ADSnapshot.DomainInformation.RIDMaster
+        'Infrastructure Master' = $ADSnapshot.DomainInformation.InfrastructureMaster
+    }
+    $DisplayAD.GroupPolicies = [ordered] @{
+        'Display Name'      = $ADSnapshot.GroupPolicies.DisplayName
+        'Creation Time'     = $ADSnapshot.GroupPolicies.CreationTime
+        'Modification Time' = $ADSnapshot.GroupPolicies.ModificationTime
+        'Gpo Status'        = $ADSnapshot.GroupPolicies.GPOStatus
+        'Wmi Filter'        = $ADSnapshot.GroupPolicies.WmiFilter
+        'Description'       = $ADSnapshot.GroupPolicies.Description
+    }
+    $DisplayAD.ForestInformation = [ordered] @{
+        'Name'                    = $ADSnapshot.ForestInformation.Name
+        'Root Domain'             = $ADSnapshot.ForestInformation.RootDomain
+        'Forest Functional Level' = $ADSnapshot.ForestInformation.ForestMode
+        'Domains Count'           = ($ADSnapshot.ForestInformation.Domains).Count
+        'Sites Count'             = ($ADSnapshot.ForestInformation.Sites).Count
+        'Domains'                 = ($ADSnapshot.ForestInformation.Domains) -join ", "
+        'Sites'                   = ($ADSnapshot.ForestInformation.Sites) -join ", "
+    }
+    $DisplayAD.OptionalFeatures = [ordered] @{
+        'Recycle Bin Enabled'                          = ''
+        #'Recycle Bin Scopes' = ''
+        'Privileged Access Management Feature Enabled' = ''
+        #'Privileged Access Management Feature Scopes' ''
+    }
+    ### Fix Optional Features
+    foreach ($Feature in $ADSnapshot.OptionalFeatures) {
+        if ($Feature.Name -eq 'Recycle Bin Feature') {
+            if ("$($Feature.EnabledScopes)" -eq '') { $DisplayAD.OptionalFeatures.'Recycle Bin Enabled' = $False }
+            else { $DisplayAD.OptionalFeatures.'Recycle Bin Enabled' = $True }
+        }
+        if ($Feature.Name -eq 'Privileged Access Management Feature') {
+            if ("$($Feature.EnabledScopes)" -eq '') { $DisplayAD.OptionalFeatures.'Privileged Access Management Feature Enabled' = $False }
+            else { $DisplayAD.OptionalFeatures.'Privileged Access Management Feature Enabled' = $True }
+        }
+    }
+    ### Fix optional features
+    $UPNSuffixList = @()
+    $UPNSuffixList += $ADSnapshot.ForestInformation.RootDomain + ' (Primary/Default UPN)'
+    $UPNSuffixList += $ADSnapshot.ForestInformation.UPNSuffixes
+    $DisplayAD.UPNSuffixes = $UPNSuffixList
+
+
+    return $DisplayAD
+}
+<#
+$ADSnapshot = Get-ActiveDirectoryCleanData
+$AD = Get-ActiveDirectoryProcessedData
+
 $ADSnapshot.DomainInformation
-Write-Color 'FSMO Information' -Color Red
-$ADSnapshot.FSMO
-Write-Color 'Forest Features' -Color Red
-$ADSnapshot.ForestFeatures | ft -a
-#$ADSnapshot.DomainInformation
-#$ADSnapshot.GroupPolicies | ft -AutoSize
+
+$ADSnapshot.RootDSE
+
+$ADSnapshot.ForestInformation
+#>
+
+
+#( + $ADSnapshot.ForestInformation.UPNSuffixes) -join ', '
+
+#foreach ($Element in $($ADSnapshot.OptionalFeatures).PSObject.Propet) {
+#    $Element
+#} #.EnabledScopes
+
+<#
+$Info = $ADSnapshot.RootDSE | Select-Object `
+@{label = 'Configuration Naming Context'; expression = { $_.configurationNamingContext }},
+@{label = 'Default Naming Context'; expression = { $_.defaultNamingContext }},
+@{label = 'DNS Host Name'; expression = { $_.dnsHostName }},
+@{label = 'Domain Controller Functionality'; expression = { $_.domainControllerFunctionality }},
+@{label = 'Domain Functionality'; expression = { $_.domainFunctionality }},
+@{label = 'Forest Functionality'; expression = { $_.forestFunctionality }},
+@{label = 'Supported LDAP Policies'; expression = { $_.supportedLDAPPolicies }},
+@{label = 'Sub Schema Subentry'; expression = { $_.subschemaSubentry }},
+@{label = 'Supported LDAP Version'; expression = { $_.supportedLDAPVersion }},
+@{label = 'Supported SASL Mechanisms'; expression = { $_.supportedSASLMechanisms }}
+
+$Info1 = Get-ADForest | Select-Object ApplicationPartitions,
+CrossForestReferences,
+DomainNamingMaster,
+Domains,
+ForestMode,
+GlobalCatalogs,
+Name,
+PartitionsContainer,
+RootDomain,
+SchemaMaster,
+Sites,
+SPNSuffixes,
+UPNSuffixes,
+
+
+#>
