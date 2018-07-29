31fbe8edcd2bdfe437532cf8f9595f0f0f8935ea
PrzemyslawKlys <pklys@evotec.pl>
2018-07-29 12:43:06 +0200

    Update

---
+++ Private/PSWordADDomain.ps1
@@ -0,0 +1,28 @@
+function Get-DomainSummary {
+    [CmdletBinding()]
+    param(
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)]$WordDocument,
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)]$Paragraph,
+        $ActiveDirectorySnapshot,
+        $Domain
+    )
+
+    $ForestName = $($ActiveDirectorySnapshot.ForestInformation.Name)
+    $ForestNameDN = $($ActiveDirectorySnapshot.RootDSE.defaultNamingContext)
+    $DomainNetBios = $ActiveDirectorySnapshot.DomainInformation.NetBIOSName
+    $DomainName = $ActiveDirectorySnapshot.DomainInformation.DNSRoot
+    $DomainDistinguishedName = $ActiveDirectorySnapshot.DomainInformation.DistinguishedName
+
+    $Text = "Following domains exists within forest $ForestName"
+    $Text0 = "Domain $DomainDistinguishedName"
+    $Text1 = "Name for fully qualified domain name (FQDN): $DomainName"
+    $Text2 = "Name for NetBIOS: $DomainNetBios"
+
+    $Paragraph = Add-WordText -WordDocument $WordDocument -Paragraph $Paragraph -Text $Text
+
+    $ListDomainInformation = $null
+    $ListDomainInformation = $ListDomainInformation | New-WordListItem -WordDocument $WordDocument -ListLevel 0 -ListItemType Bulleted -ListValue $Text0
+    $ListDomainInformation = $ListDomainInformation | New-WordListItem -WordDocument $WordDocument -ListLevel 1 -ListItemType Bulleted -ListValue $Text1
+    $ListDomainInformation = $ListDomainInformation | New-WordListItem -WordDocument $WordDocument -ListLevel 1 -ListItemType Bulleted -ListValue $Text2
+    Add-WordListItem -WordDocument $WordDocument -Paragraph $Paragraph -List $ListDomainInformation -Supress $true
+}
