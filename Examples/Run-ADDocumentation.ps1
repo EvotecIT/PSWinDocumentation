935be460abbabdfcec71b2728b4d21764aa30d0a
PrzemyslawKlys <pklys@evotec.pl>
2018-08-06 22:02:23 +0200

    Update

--- Examples/Run-ADDocumentation.ps1
+++ Examples/Run-ADDocumentation.ps1
@@ -1,10 +1,10 @@
 Import-Module PSWInDocumentation -Force
 Import-Module PSWriteWord -Force
-Import-Module C:\Support\GitHub\ImportExcel\ImportExcel.psd1 -Force
+Import-Module C:\Support\GitHub\ImportExcel\ImportExcel.psd1 -Force # Modified version of original
 Import-Module ActiveDirectory
 
 $FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-Report.docx"
 $FilePathExcel = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-Report.xlsx"
 
 Clear-Host
-Start-ActiveDirectoryDocumentation -CompanyName 'Euvic' -FilePath $FilePath -Verbose -FilePathExcel $FilePathExcel #-OpenDocument
+Start-ActiveDirectoryDocumentation -CompanyName 'Euvic' -FilePath $FilePath -Verbose -FilePathExcel $FilePathExcel -OpenDocument
