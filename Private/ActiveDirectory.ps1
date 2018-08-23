9240c06699aef54c6dcfd8369d7ba34d5d2c9bdf
PrzemyslawKlys <pklys@evotec.pl>
2018-08-23 13:15:28 +0200

    Update

--- Private/ActiveDirectory.ps1
+++ Private/ActiveDirectory.ps1
@@ -676,34 +676,6 @@ function Get-WinADDomainInformation {
     }
     if ($TypesRequired -contains [ActiveDirectory]::DomainGroupsPriviliged -or $TypesRequired -contains [ActiveDirectory]::DomainGroupMembersRecursivePriviliged) {
         Write-Verbose "Getting domain information - $Domain DomainGroupsPriviliged"
-        #$Data.DomainGroupsPriviliged = Invoke-Command -ScriptBlock {
-        <#
-            $PrivilegedGroupsSID = "S-1-5-32-544", "S-1-5-32-548", "S-1-5-32-549", "S-1-5-32-550", "S-1-5-32-551", "S-1-5-32-552", "S-1-5-32-556", "S-1-5-32-557", "S-1-5-32-573", "S-1-5-32-578", "S-1-5-32-580", "$($Data.DomainInformation.DomainSID)-512", "$($Data.DomainInformation.DomainSID)-518", "$($Data.DomainInformation.DomainSID)D-519", "$($Data.DomainInformation.DomainSID)-520"
-            $PrivilegedGroupsList = @()
-            foreach ($Group in $PrivilegedGroupsSID) {
-                $PrivilegedGroupsList += $Data.DomainGroupsFullList | Where { $_.SID.Value -eq $Group } #| Select Name, SID
-            }
-            $PrivilegedGroups = @()
-            foreach ($Group in $PrivilegedGroupsList) {
-                $User = $Data.DomainUsersFullList | Where { $_.DistinguishedName -eq $Group.ManagedBy }
-                $PrivilegedGroups += [ordered] @{
-                    'Group Name'            = $Group.Name
-                    #'Group Display Name' = $Group.DisplayName
-                    'Group Category'        = $Group.GroupCategory
-                    'Group Scope'           = $Group.GroupScope
-                    'Group SID'             = $Group.SID.Value
-                    'High Privileged Group' = if ($Group.adminCount -eq 1) { $True } else { $False }
-                    'Member Count'          = $Group.Members.Count
-                    'MemberOf Count'        = $Group.MemberOf.Count
-                    'Manager'               = $User.Name
-                    'Manager Email'         = $User.EmailAddress
-                    'Group Members'         = (Get-ADObjectFromDistingusishedName -ADCatalog $Data.DomainUsersFullList, $Data.DomainComputersFullList, $Data.DomainGroupsFullList -DistinguishedName $Group.Members -Type 'SamAccountName')
-                    'Group Members DN'      = $Group.Members
-                }
-            }
-            return Format-TransposeTable -Object $PrivilegedGroups
-        }
-        #>
         $Data.DomainGroupsPriviliged = Invoke-Command -ScriptBlock {
             $PrivilegedGroupsSID = "S-1-5-32-544", "S-1-5-32-548", "S-1-5-32-549", "S-1-5-32-550", "S-1-5-32-551", "S-1-5-32-552", "S-1-5-32-556", "S-1-5-32-557", "S-1-5-32-573", "S-1-5-32-578", "S-1-5-32-580", "$($Data.DomainInformation.DomainSID)-512", "$($Data.DomainInformation.DomainSID)-518", "$($Data.DomainInformation.DomainSID)D-519", "$($Data.DomainInformation.DomainSID)-520"
             $Groups = @()
@@ -730,9 +702,6 @@ function Get-WinADDomainInformation {
     }
     if ($TypesRequired -contains [ActiveDirectory]::DomainGroupMembersRecursiveRest) {
         Write-Verbose "Getting domain information - $Domain DomainGroupMembersRecursiveRest"
-        $Data.Test1 = Get-ObjectType -Object $Data.DomainGroupsRest
-        $Data.Test2 = Get-ObjectType -Object $Data.DomainUsersFullList, $Data.DomainComputersFullList, $Data.DomainGroupsFullList
-        $Data.Test3 = Get-ObjectType -Object $Data.DomainUsersFullList
         $Data.DomainGroupMembersRecursiveRest = Get-WinGroupMembers -Groups $Data.DomainGroupsRest -Domain $Domain -ADCatalog  $Data.DomainUsersFullList, $Data.DomainComputersFullList, $Data.DomainGroupsFullList -ADCatalogUsers $Data.DomainUsersFullList -Option 'Recursive'
     }
     if ($TypesRequired -contains [ActiveDirectory]::DomainGroupMembersRecursiveSpecial) {
--- Private/ActiveDirectoryGroups.ps1
+++ Private/ActiveDirectoryGroups.ps1
@@ -1,9 +1,8 @@
-
 function Get-WinUsers {
     param(
-        $Users,
-        $ADCatalog,
-        $ADCatalogUsers
+        [System.Object[]] $Users,
+        [System.Object[]] $ADCatalog,
+        [System.Object[]] $ADCatalogUsers
     )
     $UserList = @()
     foreach ($U in $Users) {
@@ -74,58 +73,6 @@ function Get-WinGroups {
         }
     }
     return Format-TransposeTable -Object $ReturnGroups
-}
-
-
-function Get-WinUsersFromGroup {
-    param(
-        [string] $Group,
-        [System.Object[]] $ADCatalog,
-        [System.Object[]] $ADCatalogUsers
-    )
-    $UserList = @()
-    foreach ($U in $Users) {
-        $UserList += [ordered] @{
-            'Name'                              = $U.Name
-            'UserPrincipalName'                 = $U.UserPrincipalName
-            'SamAccountName'                    = $U.SamAccountName
-            'DisplayName'                       = $U.DisplayName
-            'GivenName'                         = $U.GivenName
-            'Surname'                           = $U.Surname
-            'EmailAddress'                      = $U.EmailAddress
-            'PasswordExpired'                   = $U.PasswordExpired
-            'PasswordLastSet'                   = $U.PasswordLastSet
-            'PasswordNotRequired'               = $U.PasswordNotRequired
-            'PasswordNeverExpires'              = $U.PasswordNeverExpires
-            'Enabled'                           = $U.Enabled
-            'Manager'                           = (Get-ADObjectFromDistingusishedName -ADCatalog $ADCatalogUsers -DistinguishedName $U.Manager).Name
-            'ManagerEmail'                      = (Get-ADObjectFromDistingusishedName -ADCatalog $ADCatalogUsers -DistinguishedName $U.Manager).EmailAddress
-            'DateExpiry'                        = Convert-ToDateTime -Timestring $($U."msDS-UserPasswordExpiryTimeComputed") -Verbose
-            "DaysToExpire"                      = (Convert-TimeToDays -StartTime GET-DATE -EndTime (Convert-ToDateTime -Timestring $($U."msDS-UserPasswordExpiryTimeComputed")))
-            "AccountExpirationDate"             = $U.AccountExpirationDate
-            "AccountLockoutTime"                = $U.AccountLockoutTime
-            "AllowReversiblePasswordEncryption" = $U.AllowReversiblePasswordEncryption
-            "BadLogonCount"                     = $U.BadLogonCount
-            "CannotChangePassword"              = $U.CannotChangePassword
-            "CanonicalName"                     = $U.CanonicalName
-
-            "Description"                       = $U.Description
-            "DistinguishedName"                 = $U.DistinguishedName
-            "EmployeeID"                        = $U.EmployeeID
-            "EmployeeNumber"                    = $U.EmployeeNumber
-            "LastBadPasswordAttempt"            = $U.LastBadPasswordAttempt
-            "LastLogonDate"                     = $U.LastLogonDate
-
-            "Created"                           = $U.Created
-            "Modified"                          = $U.Modified
-            "Protected"                         = $U.ProtectedFromAccidentalDeletion
-
-            "Primary Group"                     = (Get-ADObjectFromDistingusishedName -ADCatalog $ADCatalog -DistinguishedName $U.PrimaryGroup -Type 'SamAccountName')
-            "Member Of"                         = (Get-ADObjectFromDistingusishedName -ADCatalog $ADCatalog -DistinguishedName $U.MemberOf -Type 'SamAccountName' -Splitter ', ')
-        }
-
-    }
-    return Format-TransposeTable -Object $UserList
 }
 
 function Get-WinGroupMembers {
@@ -239,102 +186,4 @@ function Get-WinGroupMembers {
         }
         return Format-TransposeTable -Object $GroupMembersDirect
     }
-}
-
-
-
-Function Get-PrivilegedGroupsMembers {
-    [CmdletBinding()]
-    param (
-        [string] $Domain,
-        [string] $DomainSID
-    )
-    $PrivilegedGroups1 = "$DomainSID-512", "$DomainSID-518", "$DomainSID-519", "$DomainSID-520" # will be only on root domain
-    $PrivilegedGroups2 = "S-1-5-32-544", "S-1-5-32-548", "S-1-5-32-549", "S-1-5-32-550", "S-1-5-32-551", "S-1-5-32-552", "S-1-5-32-556", "S-1-5-32-557", "S-1-5-32-573", "S-1-5-32-578", "S-1-5-32-580"
-
-    $SpecialGroups = @()
-    foreach ($Group in ($PrivilegedGroups1 + $PrivilegedGroups2)) {
-        Write-Verbose "Get-PrivilegedGroupsMembers - Group $Group in $Domain ($DomainSid)"
-        try {
-            $GroupInfo = Get-AdGroup -Identity $Group -Server $Domain -ErrorAction Stop
-            $GroupData = get-adgroupmember -Server $Domain -Identity $group | Sort-Object -Unique
-            $GroupDataRecursive = get-adgroupmember -Server $Domain -Identity $group -Recursive:$Recursive | Sort-Object -Unique
-            #$GroupDataRecursive | fl *
-            #$GroupData.SamAccountName #| Select * -Unique
-            #$GroupData | ft -a
-            $SpecialGroups += [ordered]@{
-                'Group Name'              = $GroupInfo.Name
-                'Group Category'          = $GroupInfo.GroupCategory
-                'Group Scope'             = $GroupInfo.GroupScope
-                'Members Count'           = Get-ObjectCount $GroupData
-                'Members Count Recursive' = Get-ObjectCount $GroupDataRecursive
-                'Members'                 = $GroupData.SamAccountName
-                'Members Recursive'       = $GroupDataRecursive.SamAccountName
-            }
-        } catch {
-            Write-Verbose "Get-PrivilegedGroupsMembers - Error on Group $Group in $Domain ($DomainSid)"
-        }
-    }
-    return $SpecialGroups.ForEach( {[PSCustomObject]$_})
-}
-function Get-WinCustomGroupInformation {
-    [CmdletBinding()]
-    param (
-        [string] $Domain,
-        [string[]] $Groups
-    )
-    $SpecialGroups = @()
-    $AllGroups = Get-ADGroup -Server $Domain -Filter *
-    foreach ($Group in $AllGroups) {
-        $GroupInfo = Get-AdGroup -Identity $Group -Server $Domain -ErrorAction Stop
-        $GroupData = get-adgroupmember -Server $Domain -Identity $group | Sort-Object -Unique
-        $GroupDataRecursive = get-adgroupmember -Server $Domain -Identity $group -Recursive:$Recursive | Sort-Object -Unique
-        $SpecialGroups += [ordered]@{
-            'Group Name'              = $GroupInfo.Name
-            'Group Category'          = $GroupInfo.GroupCategory
-            'Group Scope'             = $GroupInfo.GroupScope
-            'Members Count'           = Get-ObjectCount $GroupData
-            'Members Count Recursive' = Get-ObjectCount $GroupDataRecursive
-            'Members'                 = $GroupData.SamAccountName
-            'Members Recursive'       = $GroupDataRecursive.SamAccountName
-            'Group SID'               = $GroupInfo.SID
-        }
-    }
-    return $SpecialGroups.ForEach( {[PSCustomObject]$_}) | Sort-Object 'Group SID'
-}
-function Get-SpecialGroups {
-    param (
-        $Groups,
-        $Type
-    )
-    $PrivilegedGroups2 = "S-1-5-32-544", "S-1-5-32-548", "S-1-5-32-549", "S-1-5-32-550", "S-1-5-32-551", "S-1-5-32-552", "S-1-5-32-556", "S-1-5-32-557", "S-1-5-32-573", "S-1-5-32-578", "S-1-5-32-580"
-    $PrivilegedGroups1 = "$DomainSID-512", "$DomainSID-518", "$DomainSID-519", "$DomainSID-520" # will be only on root domain
-
-    $Groups = $Groups | Where { ("$($_.'Group SID')").length -eq 12 }
-
-    If ($Type -eq 0) {
-        $Yellow = $Groups | Where {$PrivilegedGroups2 -NotContains $_.'Group SID'}
-    } elseif ($Type -eq 1) {
-        $Yellow = $Groups | Where {$PrivilegedGroups2 -contains $_.'Group SID'}
-    } elseif ($Type -eq 2) {
-        #$Yellow = $Groups | Where {$PrivilegedGroups1 -c}
-    }
-    return $Yellow
-
-
-    #$PrivGroups = @()
-    foreach ($Group in $Groups) {
-        foreach ($PrivGroup in $PrivilegedGroups2) {
-            if ($NotInScope) {
-                if ($Group.'Group SID' -ne $PrivGroup) {
-                    #  $PrivGroups += $Group
-                }
-            } else {
-                if ($Group.'Group SID' -eq $PrivGroup) {
-                    #   $PrivGroups += $Group
-                }
-            }
-        }
-    }
-    # return $PrivGroups
 }
