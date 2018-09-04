a336227846694e978a0f4eb14518e90cb533b27f
PrzemyslawKlys <pklys@evotec.pl>
2018-09-04 15:25:48 +0200

    Update

--- PSWinDocumentation.psd1
+++ PSWinDocumentation.psd1
@@ -43,7 +43,7 @@
     # ProcessorArchitecture = ''
 
     # Modules that must be imported into the global environment prior to importing this module
-    RequiredModules   = @('PSWriteWord', 'PSWriteExcel', 'PSWriteColor', 'PSSharedGoods', 'AWSPowerShell')
+    RequiredModules   = @('PSWriteWord', 'PSWriteExcel', 'PSWriteColor', 'PSSharedGoods', 'AWSPowerShell', 'DbaTools')
 
     # Assemblies that must be loaded prior to importing this module
     # RequiredAssemblies = @()
--- Private/SQL.ps1
+++ Private/SQL.ps1
@@ -24,20 +24,6 @@ function Send-SqlInsert {
     return $ReturnData
 }
 
-function Display-DataInVerbose {
-    [CmdletBinding()]
-    param(
-        [Object] $Object
-    )
-    foreach ($O in $Object) {
-        foreach ($E in $O.PSObject.Properties) {
-            $FieldName = $E.Name
-            $FieldValue = $E.Value
-            Write-Verbose "Display-DataInVerbose - FieldName: $FieldName FieldValue: $FieldValue"
-        }
-    }
-}
-
 function New-SqlQuery {
     [CmdletBinding()]
     param (
