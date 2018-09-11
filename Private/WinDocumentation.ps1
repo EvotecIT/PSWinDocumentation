0933632363e5f7014b46c1ec6971a64a6594b98b
PrzemyslawKlys <pklys@evotec.pl>
2018-09-11 20:02:53 +0200

    Update

--- Private/WinDocumentation.ps1
+++ Private/WinDocumentation.ps1
@@ -136,33 +136,3 @@ function New-ADDocumentBlock {
     }
     if ($WordDocument) { return $WordDocument } else { return }
 }
-
-function Start-ActiveDirectoryDocumentation {
-    [CmdletBinding()]
-    param (
-        [string] $FilePath,
-        [string] $FilePathExcel,
-        [switch] $CleanDocument,
-        [string] $CompanyName = 'Evotec',
-        [switch] $OpenDocument,
-        [switch] $OpenExcel
-    )
-    #Write-Warning 'This is legacy command. Use Start-Documentation instead.'
-    # Left here for legacy reasons.
-    $Document = $Script:Document
-    $Document.Configuration.Prettify.CompanyName = $CompanyName
-    if ($CleanDocument) {
-        $Document.Configuration.Prettify.UseBuiltinTemplate = $false
-    }
-    $Document.Configuration.Options.OpenDocument = $OpenDocument
-    $Document.Configuration.Options.OpenExcel = $OpenExcel
-    $Document.DocumentAD.FilePathWord = $FilePath
-    if ($FilePathExcel) {
-        $Document.DocumentAD.ExportExcel = $true
-        $Document.DocumentAD.FilePathExcel = $FilePathExcel
-    } else {
-        $Document.DocumentAD.ExportExcel = $false
-    }
-
-    Start-Documentation -Document $Document
-}
