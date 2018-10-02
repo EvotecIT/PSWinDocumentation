e1ed78d2c16d5ac9b5aa2ea6009dcb867221d41e
PrzemyslawKlys <pklys@evotec.pl>
2018-10-02 17:23:23 +0200

    Example of using Get-WinAdForest/Domain

--- Examples/Run-Manual-AD.ps1
+++ Examples/Run-Manual-AD.ps1
@@ -1,9 +1,14 @@
-Import-Module PSWinDocumentation
+Clear-Host
+Import-Module PSWinDocumentation -Force
 Import-Module PSSharedGoods
 Import-Module ActiveDirectory
 
 $Forest = Get-WinADForestInformation -Verbose
-$Forest
-#$Forest.FoundDomains
-#$Domain = Get-WinADDomainInformation -Domain 'ad.evotec.xyz'
-#$Domain
+$Forest.FoundDomains.'ad.evotec.pl'
+$Forest.FoundDomains.'ad.evotec.pl'.DomainFineGrainedPoliciesUsers | Format-Table -AutoSize
+$Forest.FoundDomains.'ad.evotec.xyz'.DomainRIDs | Format-Table -AutoSize
+
+$Domain = Get-WinADDomainInformation -Domain 'ad.evotec.pl' -Verbose
+$Domain.DomainFineGrainedPolicies
+$Domain.DomainFineGrainedPoliciesUsers | Format-Table -AutoSize
+$Domain.DomainFineGrainedPoliciesUsersExtended | format-Table -AutoSize
