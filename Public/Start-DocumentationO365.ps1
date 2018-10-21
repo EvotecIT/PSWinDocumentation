bcd380a5af1d18cf7059a9f08c977cce77cb3744
PrzemyslawKlys <pklys@evotec.pl>
2018-10-21 20:11:37 +0200

    O365 Updates

--- Public/Start-DocumentationO365.ps1
+++ Public/Start-DocumentationO365.ps1
@@ -1,59 +1,35 @@
 function Start-DocumentationO365 {
     [CmdletBinding()]
     param(
-        $Document
+        [Object] $Document
     )
+    $TypesRequired = Get-TypesRequired -Sections $Document.DocumentOffice365.Sections
+    $DataSections = Get-ObjectKeys -Object $Document.DocumentOffice365.Sections
 
-    $CheckCredentials = Test-ConfigurationCredentials -Configuration $Document.DocumentOffice365.Configuration
+    $TimeDataOnly = [System.Diagnostics.Stopwatch]::StartNew() # Timer Start
 
-    if ($CheckCredentials) {
-        if ($Document.DocumentOffice365.Configuration.O365ExchangeUse) {
-            $Session = Connect-WinExchange -SessionName $Document.DocumentOffice365.Configuration.O365ExchangeSessionName `
-                -ConnectionURI $Document.DocumentOffice365.Configuration.O365ExchangeURI `
-                -Authentication $Document.DocumentOffice365.Configuration.O365ExchangeAuthentication `
-                -Username $Document.DocumentOffice365.Configuration.O365Username `
-                -Password $Document.DocumentOffice365.Configuration.O365Password `
-                -AsSecure:$Document.DocumentOffice365.Configuration.O365PasswordAsSecure `
-                -FromFile:$Document.DocumentOffice365.Configuration.O365PasswordFromFile -Verbose
+    $DataInformation = @()
+    $DataInformation += Get-WinServiceData -Credentials $Document.DocumentOffice365.Services.Office365.Credentials `
+        -Service $Document.DocumentOffice365.Services.Office365.Azure `
+        -TypesRequired $TypesRequired `
+        -Type 'Azure'
+    $DataInformation += Get-WinServiceData -Credentials $Document.DocumentOffice365.Services.Office365.Credentials `
+        -Service $Document.DocumentOffice365.Services.Office365.AzureAD `
+        -TypesRequired $TypesRequired `
+        -Type 'AzureAD'
+    $DataInformation += Get-WinServiceData -Credentials $Document.DocumentOffice365.Services.Office365.Credentials `
+        -Service $Document.DocumentOffice365.Services.Office365.ExchangeOnline `
+        -TypesRequired $TypesRequired `
+        -Type 'ExchangeOnline'
 
-            # Failed connecting to exchange
-            if (-not $Session) { return }
+    $TimeDataOnly.Stop()
 
-            $CurrentVerbosePreference = $VerbosePreference; $VerbosePreference = 'SilentlyContinue' # weird but -Verbose:$false doesn't do anything below
-            $ImportedSession = Import-PSSession -Session $Session -AllowClobber -DisableNameChecking -Prefix 'O365' -Verbose:$false
-            $VerbosePreference = $CurrentVerbosePreference
-        }
 
-        if ($Document.DocumentOffice365.Configuration.O365AzureADUse) {
-            $SessionAzure = Connect-WinAzure -SessionName $Document.DocumentOffice365.Configuration.O365AzureSessionName `
-                -Username $Document.DocumentOffice365.Configuration.O365Username `
-                -Password $Document.DocumentOffice365.Configuration.O365Password `
-                -AsSecure:$Document.DocumentOffice365.Configuration.O365PasswordAsSecure `
-                -FromFile:$Document.DocumentOffice365.Configuration.O365PasswordFromFile -Verbose
-        }
+    $TimeDocuments = [System.Diagnostics.Stopwatch]::StartNew() # Timer Start
+    ### Starting WORD
 
-        $TypesRequired = Get-TypesRequired -Sections $Document.DocumentOffice365.Sections
-        $DataSections = Get-ObjectKeys -Object $Document.DocumentOffice365.Sections
+    if ($DataInformation) {
 
-        $TimeDataOnly = [System.Diagnostics.Stopwatch]::StartNew() # Timer Start
-
-        if ($Document.DocumentOffice365.Configuration.O365ExchangeUse) {
-            $CheckAvailabilityCommands = Test-AvailabilityCommands -Commands 'Get-O365MailContact', 'Get-O365CalendarProcessing'
-            if ($CheckAvailabilityCommands -notcontains $false) {
-                $DataInformationO365 = Get-WinO365Exchange -TypesRequired $TypesRequired
-            }
-        } else {
-            $DataInformationO365 = $null
-        }
-        if ($Document.DocumentOffice365.Configuration.O365AzureADUse) {
-            $DataInformationAzure = Get-WinO365Azure -TypesRequired $TypesRequired
-        } else {
-            $DataInformationAzure = $null
-        }
-        $TimeDataOnly.Stop()
-
-        $TimeDocuments = [System.Diagnostics.Stopwatch]::StartNew() # Timer Start
-        ### Starting WORD
         if ($Document.DocumentOffice365.ExportWord) {
             $WordDocument = Get-DocumentPath -Document $Document -FinalDocumentLocation $Document.DocumentOffice365.FilePathWord
         }
@@ -62,12 +38,12 @@ function Start-DocumentationO365 {
         }
 
         ### Start Sections
-        foreach ($DataInformation in $($DataInformationO365 + $DataInformationAzure)) {
+        foreach ($Data in $DataInformation) {
             foreach ($Section in $DataSections) {
                 $WordDocument = New-DataBlock `
                     -WordDocument $WordDocument `
                     -Section $Document.DocumentOffice365.Sections.$Section `
-                    -Forest $DataInformation `
+                    -Forest $Data `
                     -Excel $ExcelDocument `
                     -SectionName $Section `
                     -Sql $Document.DocumentOffice365.ExportSQL
@@ -83,6 +59,7 @@ function Start-DocumentationO365 {
         if ($Document.DocumentOffice365.ExportExcel) {
             $ExcelData = Save-ExcelDocument -ExcelDocument $ExcelDocument -FilePath $Document.DocumentOffice365.FilePathExcel -OpenWorkBook:$Document.Configuration.Options.OpenExcel
         }
+
         $TimeDocuments.Stop()
         $TimeTotal.Stop()
         Write-Verbose "Time to gather data: $($TimeDataOnly.Elapsed)"
